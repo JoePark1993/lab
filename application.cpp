7a2704edaff35a0ed38f5e4aa2f6e0fc10fafa04
JoePark1993 <32422763+JoePark1993@users.noreply.github.com>
2018-03-07 19:50:34 -0800

    Add files via upload

---
+++ application.cpp
@@ -0,0 +1,388 @@
+#include "application.h"
+
+#include <iostream>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+
+#ifndef __APPLE__
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#else
+#include <GLUT/glut.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#endif
+
+using namespace std;
+enum { NONE, AMBIENT, DIFFUSE, SPECULAR, NUM_MODES };
+
+
+void draw_grid(int dim);
+void draw_obj(obj *o, const gl_image_texture_map& textures);
+
+struct Particle
+{
+	vec3 x;
+	vec3 v;
+	vec3 f;
+	vec3 d(0,0,0);
+	float m;
+	vec3 color;
+	void Euler_Step(float h) {
+		v = v + (h/m)*f;
+		x = x + h*v;
+	}
+	void Reset_Forces(){
+		f[0] = 0;
+		f[1] = 0;
+		f[2] = 0;
+	}
+	void Handle_Collision(float damping, float coeff_resititution){
+		if (x[1] < 0) {
+			x[1] = 0;
+			if(v[1] < 0) {
+				v[1] = -v[1] * coeff_resititution;
+				v[2] = v[2] * damping;
+				v[0] = v[0] * damping;
+			}
+		}
+	}
+};
+
+vector<Particle> particle;
+
+float random(float k,float l){
+	float r = k + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/(l-k)));
+
+	return r;
+}
+
+void Add_Particles(int n){
+	for( int i = 0; i < n ; i++){
+		Particle p;
+		p.m = 1;
+		vec3 t;
+		vec3 t2;
+		//float r1=static_cast<float>(rand()) / static_cast<float>(RAND_MAX/(0.2+0.2));
+		//float r2=static_cast<float>(rand()) / static_cast<float>(RAND_MAX/(10.0-1.0));
+		
+		
+		t[0] = random(-0.2,0.2);
+		t[1] = 0.05;
+		t[2] = random(-0.2,0.2);
+		p.x = t;
+		t2[0] = 10*p.x[0];
+		t2[1] = random(1,10);
+		t2[2] = 10*p.x[2];
+		p.v = t2;
+		p.color[0] = 1;
+		p.color[1] = 1;
+		p.color[2] = 0;
+		
+		particle.push_back(p);
+	}
+}
+
+void set_pixel(int x, int y, float col[3])
+{
+    // write a 1x1 block of pixels of color col to framebuffer
+    // coordinates (x, y)
+    //glRasterPos2i(x, y);
+    //glDrawPixels(1, 1, GL_RGB, GL_FLOAT, col);
+
+    // use glVertex instead of glDrawPixels (faster)
+    glBegin(GL_POINTS);
+    glColor3fv(col);
+    glVertex2f(x, y);
+    glEnd();
+}
+
+application::application()
+    : raytrace(false), rendmode(SPECULAR), paused(false), sim_t(0.0),draw_volcano(true),h(0.015)
+{
+}
+
+application::~application()
+{
+}
+
+// triggered once after the OpenGL context is initialized
+void application::init_event()
+{
+	Add_Particles(10);
+    cout << "CAMERA CONTROLS: \n  LMB: Rotate \n  MMB: Move \n  RMB: Zoom" << endl;
+    cout << "KEYBOARD CONTROLS: \n";
+    cout << "  'p': Pause simulation\n";
+    cout << "  'v': Toggle draw volcano" << endl;
+
+    const GLfloat ambient[] = { 0.0, 0.0, 0.0, 1.0 };
+    const GLfloat diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+    const GLfloat specular[] = { 1.0, 1.0, 1.0, 1.0 };
+
+    // enable a light
+    glLightfv(GL_LIGHT1, GL_AMBIENT, ambient);
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, diffuse);
+    glLightfv(GL_LIGHT1, GL_SPECULAR, specular);
+    glEnable(GL_LIGHT1);
+
+    // set global ambient lighting
+    GLfloat amb[] = { 0.4, 0.4, 0.4, 1.0 };
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, amb);
+
+    // enable depth-testing, colored materials, and lighting
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glDisable(GL_COLOR_MATERIAL);
+    glEnable(GL_LIGHTING);
+
+    // normalize normals so lighting calculations are correct
+    // when using GLUT primitives
+    glEnable(GL_RESCALE_NORMAL);
+
+    // enable smooth shading
+    glShadeModel(GL_SMOOTH);
+
+    glClearColor(0,0,0,0);
+
+    set_camera_for_box(vec3(-3,-2,-3),vec3(3,5,3));
+
+    t.reset();
+    o.load("crater.obj");
+
+    // loads up all the textures referenced by the .mtl file
+    const std::map<std::string, obj::material>& mats = o.get_materials();
+    for (std::map<std::string, obj::material>::const_iterator i = mats.begin();
+        i != mats.end(); ++i
+        )
+    {
+        if (!i->second.map_kd.empty()) {
+            string filename = i->second.map_kd;
+
+            // add texture if we have not already loaded it
+            if (texs.find(filename) == texs.end()) {
+                gl_image_texture *tex = new gl_image_texture();
+                if (tex->load_texture(filename) != SUCCESS) {
+                    cout << "could not load texture file: " << filename << endl;
+                    exit(0);
+                }
+                texs[filename] = tex;
+            }
+        }
+    }
+}
+
+// triggered each time the application needs to redraw
+void application::draw_event()
+{
+    apply_gl_transform();
+
+    const GLfloat light_pos1[] = { 0.0, 10.0, 0.0, 1 };
+    glLightfv(GL_LIGHT1, GL_POSITION, light_pos1);
+
+    if (!paused) {
+        //
+        //ADD NEW PARTICLES
+        //
+        //
+        // SIMULATE YOUR PARTICLE HERE.
+        //
+        //
+        //
+        // UPDATE THE COLOR OF THE PARTICLE DYNAMICALLY
+        //
+        Add_Particles(20);
+        for(auto& p : particle){
+			p.Reset_Forces();
+			p.f[0] = 0;
+			p.f[1] = -9.8*p.m;
+			p.f[2] = 0;
+			p.Euler_Step(h);
+			p.Handle_Collision(0.5,0.5);
+			
+		}
+    }
+
+    glLineWidth(2.0);
+    glEnable(GL_COLOR_MATERIAL);
+    glBegin(GL_LINES);
+        //
+        //
+        // DRAW YOUR PARTICLE USING GL_LINES HERE
+        //
+        // glVertex3f(...) endpoint 1
+        // glVertex3f(...) endpoint 2
+        //
+        //
+    for(auto& p : particle){
+		vec3 t = p.x + p.v*0.04;
+		glVertex3f(p.x[0],p.x[1],p.x[2]);
+		glVertex3f(t[0],t[1],t[2]);
+	}
+    glEnd();
+
+    // draw the volcano
+    if(draw_volcano){
+        glEnable(GL_LIGHTING);
+        glPushMatrix();
+        glScalef(0.2,0.3,0.2);
+        draw_obj(&o, texs);
+        glPopMatrix();
+        glDisable(GL_LIGHTING);
+    }
+
+
+    glColor3f(0.15, 0.15, 0.15);
+    draw_grid(40);
+
+    //
+    // This makes sure that the frame rate is locked to close to 60 fps.
+    // For each call to draw_event you will want to run your integrate for 0.015s
+    // worth of time.
+    //
+    float elap = t.elapsed();
+    if (elap < h) {
+        usleep(1e6*(h-elap));
+    }
+    t.reset();
+}
+
+// triggered when mouse is clicked
+void application::mouse_click_event(int button, int button_state, int x, int y)
+{
+}
+
+// triggered when mouse button is held down and the mouse is
+// moved
+void application::mouse_move_event(int x, int y)
+{
+}
+
+// triggered when a key is pressed on the keyboard
+void application::keyboard_event(unsigned char key, int x, int y)
+{
+
+    if (key == 'r') {
+        sim_t = 0;
+    } else if (key == 'p') {
+        paused = !paused;
+    } else if (key == 'v') {
+        draw_volcano=!draw_volcano;
+    } else if (key == 'q') {
+        exit(0);
+    }
+}
+
+void draw_grid(int dim)
+{
+    glLineWidth(2.0);
+
+
+    //
+    // Draws a grid along the x-z plane
+    //
+    glLineWidth(1.0);
+    glBegin(GL_LINES);
+
+    int ncells = dim;
+    int ncells2 = ncells/2;
+
+    for (int i= 0; i <= ncells; i++)
+    {
+        int k = -ncells2;
+        k +=i;
+        glVertex3f(ncells2,0,k);
+        glVertex3f(-ncells2,0,k);
+        glVertex3f(k,0,ncells2);
+        glVertex3f(k,0,-ncells2);
+    }
+    glEnd();
+
+    //
+    // Draws the coordinate frame at origin
+    //
+    glPushMatrix();
+    glScalef(1.0, 1.0, 1.0);
+    glBegin(GL_LINES);
+
+    // x-axis
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex3f(0.0, 0.0, 0.0);
+    glVertex3f(1.0, 0.0, 0.0);
+
+    // y-axis
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f(0.0, 0.0, 0.0);
+    glVertex3f(0.0, 1.0, 0.0);
+
+    // z-axis
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex3f(0.0, 0.0, 0.0);
+    glVertex3f(0.0, 0.0, 1.0);
+    glEnd();
+    glPopMatrix();
+}
+
+void draw_obj(obj *o, const gl_image_texture_map& textures)
+{
+    glDisable(GL_COLOR_MATERIAL);
+
+    // draw each polygon of the mesh
+    size_t nfaces = o->get_face_count();
+    for (size_t i = 0; i < nfaces; ++i)
+    {
+        const obj::face& f = o->get_face(i);
+
+        // sets the material properties of the face
+        const obj::material& mat = o->get_material(f.mat);
+        if (!mat.map_kd.empty()) {
+            gl_image_texture_map::const_iterator it = textures.find(mat.map_kd);
+            if (it != textures.end()) {
+                gl_image_texture* tex = it->second;
+                tex->bind();
+            }
+            GLfloat mat_amb[] = { 1, 1, 1, 1 };
+            GLfloat mat_dif[] = { mat.kd[0], mat.kd[1], mat.kd[2], 1 };
+            GLfloat mat_spec[] = { mat.ks[0], mat.ks[1], mat.ks[2], 1 };
+            glMaterialfv(GL_FRONT, GL_AMBIENT, mat_amb);
+            glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_dif);
+            glMaterialfv(GL_FRONT, GL_SPECULAR, mat_spec);
+        } else {
+            GLfloat mat_amb[] = { mat.ka[0], mat.ka[1], mat.ka[2], 1 };
+            GLfloat mat_dif[] = { mat.kd[0], mat.kd[1], mat.kd[2], 1 };
+            GLfloat mat_spec[] = { mat.ks[0], mat.ks[1], mat.ks[2], 1 };
+            glMaterialfv(GL_FRONT, GL_AMBIENT, mat_amb);
+            glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_dif);
+            glMaterialfv(GL_FRONT, GL_SPECULAR, mat_spec);
+        }
+        glMaterialf(GL_FRONT, GL_SHININESS, mat.ns);
+
+        if (!glIsEnabled(GL_TEXTURE_2D)) glEnable(GL_TEXTURE_2D);
+
+        // draws a single polygon
+        glBegin(GL_POLYGON);
+        for (size_t j = 0; j < f.vind.size(); ++j)
+        {
+            // vertex normal
+            if (f.nind.size() == f.vind.size()) {
+                const float *norm = o->get_normal(f.nind[j]);
+                glNormal3fv(norm);
+            }
+
+            // vertex UV coordinate
+            if (f.tex.size() > 0) {
+                const float* tex = o->get_texture_indices(f.tex[j]);
+                glTexCoord2fv(tex);
+            }
+
+            // vertex coordinates
+            const float *vert = o->get_vertex(f.vind[j]);
+            glVertex3fv(vert);
+        }
+        glEnd();
+    }
+    glDisable(GL_TEXTURE_2D);
+    glEnable(GL_COLOR_MATERIAL);
+}
